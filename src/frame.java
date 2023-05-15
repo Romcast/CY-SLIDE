637f1bc6387bbca0fd877dc684ec9e972174dd28
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-05-15 16:43:24 +0200

    Add files via upload

---
+++ src/frame.java
@@ -0,0 +1,10 @@
+
+public class frame {
+	private int x;
+	private int y;
+	private boolean movable;
+	private int value;
+	public boolean isMovable() {}
+	public boolean isVoid() {}
+	public boolean exist() {}
+}
---
+++ src/level.java
@@ -0,0 +1,16 @@
+
+public class level {
+	private int number;
+	private int score;
+	private player player;
+	private frame[][] grid;
+	private frame[][] finalGrid;
+	public boolean isSolved() {}
+	public void shuffle() {}
+	public void randomShuffle() {}
+	public boolean wellShuffled() {}
+	public boolean accessible() {}
+	public boolean isSolvable() {}
+	public int solve() {}
+	public frame[][] moveFrame(frame frame, int direction ) {}
+}
