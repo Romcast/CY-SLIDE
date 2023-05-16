022862ebde6154dca91d89b522fccb3cc42edbd8
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-05-16 14:00:49 +0200

    Delete src directory

--- src/player.java
+++
@@ -1,8 +0,0 @@
-
-public class player {
-	private String pseudo;
-	private int levelMax;
-	level currentLevel;
-	public void play(level level) {}
-	public level createLevel(initialLevel initialLevel) {}
-}
