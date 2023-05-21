e27205d95933d7764d1c0387fbf95daf2ea396ea
ren simon <118524337+rensimon1@users.noreply.github.com>
2023-05-21 10:55:02 +0200

    Update Screen.java

--- src/com/slidingPuzzle/Screen.java
+++ src/com/slidingPuzzle/Screen.java
@@ -17,36 +17,12 @@ import javafx.scene.control.Button;
 
 public class Screen extends Application{   
 	
-	/*private Cell c1 = new Cell(0, 0, 1, CellType.GameCell);
-	private Cell c2 = new Cell(0, 1, 2, CellType.GameCell);
-	private Cell c3 = new Cell(0, 2, 3, CellType.GameCell);
-	private Cell c4 = new Cell(1, 0, 4, CellType.GameCell);
-	private Cell c5 = new Cell(1, 1, 5, CellType.GameCell);
-	private Cell c6 = new Cell(1, 2, 6, CellType.GameCell);
-	private Cell c7 = new Cell(2, 0, 7, CellType.GameCell);
-	private Cell c8 = new Cell(2, 1, 8, CellType.GameCell);
-	private Cell c0 = new Cell(2, 2, 0, CellType.EmptyCell);
-	private Cell[][] cells = {{c1, c2, c3}, {c4, c5, c6}, {c7, c8, c0}};
-	private Grid grid = new Grid(cells,cells);*/
-	
 	File fileLevel = new File("H:/Documents/cours/projet/SLIDE-main/levels/level_1.csv");
     Grid level = new Grid(fileLevel);
-	/*String fileName = "../levels/level_1.csv";
-    File fileLevel = new File(fileName);
-    Grid level = new Grid(fileLevel);*/
     
 	@Override     
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("CY SLIDE");
-		// Definition d'un bouton avec action sur clic  
-		
-		//InitialLevel level1=new InitialLevel("level_1.csv",1);
-		//int nbLine=level1.goal.getNbLine();
-		//int nbColumn=level1.goal.getNbColumn();
-		//Menu menu=new Menu();
-		
-		
-		
 		
 		//Text level = new Text(20, 100, "Niveau");
 		GridPane gridpane= new GridPane();
