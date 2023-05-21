6ae6bcf957caa2f0e23c8faaa16424e4be73ebc3
ren simon <118524337+rensimon1@users.noreply.github.com>
2023-05-21 17:40:25 +0200

    Add files via upload

--- src/com/slidingPuzzle/Screen.java
+++ src/com/slidingPuzzle/Screen.java
@@ -17,25 +17,60 @@ import javafx.scene.control.Button;
 
 public class Screen extends Application{   
 	
+	
+	
 	File fileLevel = new File("H:/Documents/cours/projet/SLIDE-main/levels/level_1.csv");
     Grid level = new Grid(fileLevel);
+	/*String fileName = "../levels/level_1.csv";
+    File fileLevel = new File(fileName);
+    Grid level = new Grid(fileLevel);*/
     
 	@Override     
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("CY SLIDE");
-		
 		//Text level = new Text(20, 100, "Niveau");
 		GridPane gridpane= new GridPane();
 		for (int i=0; i<level.getNbRows(); i++) {
 			for (int j=0; j<level.getNbColumns();j++) {
-				Integer value=level.getGrid()[i][j].getValue();
-				if (value==null) {
-					value=0;
-				}
-				Button button=new Button(value.toString());
+				switch(this.level.getGrid()[i][j].getType())
+                {
+                case EmptyCell: 
+                	String buttonText=" ";
+                	Button button=new Button(buttonText);
+                	button.setPrefSize(100, 100);
+    				button.setMinSize(70, 70);
+    				button.setMaxSize(150,150);
+                	gridpane.add(button,j,i);
+                	
+                    break;
+                case UnexistantCell: 
+                	buttonText="-";
+                	button=new Button(buttonText);
+                	button.setPrefSize(100, 100);
+    				button.setMinSize(70, 70);
+    				button.setMaxSize(150,150);
+                	gridpane.add(button,j,i);
+                    break;
+                case GameCell: 
+                    buttonText=this.level.getGrid()[i][j].getValue().toString() ;
+                    button=new Button(buttonText);
+                    button.setPrefSize(100, 100);
+    				button.setMinSize(70, 70);
+    				button.setMaxSize(150,150);
+                    gridpane.add(button,j,i);
+                    break;
+                default:
+                    break;
+                }
+				
+				
+				//button.setOnDrangDetected()
+				/*button.setPrefSize(100, 100);
+				button.setMinSize(70, 70);
+				button.setMaxSize(150,150);
 				if (value==0) {
 					button.setText(" ");
-				}
+				}*/
 				
 				/*button.setOnAction(event ->{
 					int column=gridpane.getColumnIndex(button);
@@ -43,41 +78,74 @@ public class Screen extends Application{
 					System.out.println(row+","+column);
 					});*/
 
-				gridpane.add(button,j,i);
+				
 			}
 		}
 
-
-		
-
-		//grid.add(btn, 0, 0);
 		Button shuffle=new Button("shuffle");
 		shuffle.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				level.shuffle();
+				
+				level.stepByStepShuffle();
 				updateGrid(gridpane,level);
 			}
 		});
 		
-		VBox root=new VBox(gridpane,shuffle);
-		Scene scene=new Scene(root,400,400);
+		Button randomShuffle=new Button("Random shuffle");
+		randomShuffle.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				level.randomShuffled();
+				updateGrid(gridpane,level);
+			}
+		});
+		
+		
+		
+		
+		
+		VBox root=new VBox(gridpane,shuffle, randomShuffle);
+		
+		Scene scene=new Scene(root,600,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		}
 		
+	
+	
+	
+	
 	public void updateGrid(GridPane gridpane, Grid grid) {
 		for (int i=0; i<grid.getNbRows(); i++) {
 			for (int j=0; j<grid.getNbColumns();j++) {
-				Integer value=grid.getGrid()[i][j].getValue();//grid.getGrid() returns the shuffled cell[][] cells
-				if (value==null) {
-					value=0;
-				}
-				Button button = (Button) gridpane.getChildren().get(i*grid.getNbColumns()+j); // getChildren() returns a list
+				//Integer value=grid.getGrid()[i][j].getValue();//grid.getGrid() returns the shuffled cell[][] cells
+				switch(grid.getGrid()[i][j].getType())
+                {
+                case EmptyCell: 
+                	String buttonText=" ";
+                	Button button = (Button) gridpane.getChildren().get(i*grid.getNbColumns()+j); // getChildren() returns a list
+    				button.setText(buttonText);
+                    break;
+                case UnexistantCell: 
+                	buttonText="-";
+                	button = (Button) gridpane.getChildren().get(i*grid.getNbColumns()+j); // getChildren() returns a list
+    				button.setText(buttonText);
+                    break;
+                case GameCell: 
+                    buttonText=this.level.getGrid()[i][j].getValue().toString() ;
+                    button = (Button) gridpane.getChildren().get(i*grid.getNbColumns()+j); // getChildren() returns a list
+    				button.setText(buttonText);
+                    break;
+                default:
+                    break;
+                }
+				//System.out.println(value);
+				/*Button button = (Button) gridpane.getChildren().get(i*grid.getNbColumns()+j); // getChildren() returns a list
 				button.setText(value.toString());
 				if (value==0) {
 					button.setText(" ");
-				}
+				}*/
 			}
 		}
 	}
@@ -87,7 +155,6 @@ public class Screen extends Application{
 	public static void main(String[] args) {
 		launch(args);
 		}
-		
 	} 
 
 
