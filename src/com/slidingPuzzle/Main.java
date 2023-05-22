42d76f1d1d7ee25d641642a5f9b1c9fd7900b974
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-05-22 15:49:31 +0200

    Update Main.java

--- src/com/slidingPuzzle/Main.java
+++ src/com/slidingPuzzle/Main.java
@@ -45,7 +45,7 @@ public class Main extends Application {
 	}
 	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException, ClassNotFoundException {
 		try {
 			BorderPane root = new BorderPane();
 			
