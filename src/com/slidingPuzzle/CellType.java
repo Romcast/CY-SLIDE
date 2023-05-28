b8945878a1cb20da6cc9b9e8b7e6934c19681d2b
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-05-28 20:41:10 +0200

    java docCellType.java

--- src/com/slidingPuzzle/CellType.java
+++ src/com/slidingPuzzle/CellType.java
@@ -1,2 +1,6 @@
-
+/**
+ * It gives you the different type of cell EmptyCell is the one in which you can slide the GameCell and UnexistantCell is a cell that is non movable
+ * @author CYTech Student
+ *
+ */
 public enum CellType {EmptyCell,UnexistantCell,GameCell;}
