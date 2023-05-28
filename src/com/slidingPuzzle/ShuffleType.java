4d5de1c05b8b82dbee9de0d2a2f71e8417566996
sarahbahhar <129052493+sarahbahhar@users.noreply.github.com>
2023-05-28 20:48:15 +0200

    javadoc ShuffleType.java

--- src/com/slidingPuzzle/ShuffleType.java
+++ src/com/slidingPuzzle/ShuffleType.java
@@ -1,1 +1,6 @@
+/**
+ * This enumeration present the different type of shuffle their is the Step by step one and the random one
+ * @author CYTech Student
+ *
+ */
 public enum ShuffleType {StepByStep,Random;}
